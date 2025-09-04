7fcb65eb7d2f83e17f3e4c0ac21c0bd4143c578e
kadamatharva2722 <kadamatharva04@gmail.com>
2025-09-04 22:35:45 +0530

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+//Schema
+const userSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true,
+    },
+    last_name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    gender: {
+        type: String,
+        required: true,
+    },
+},
+    { timestamps: true }
+);
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
